8978e7ebb3c0e8ca6918e16e95c7144a7366dd62
austineamonn <nicolasa@grinnell.edu>
2025-06-18 05:18:26 -0700

    removed great people and added unique merchant.

--- earth_kingdom_v2/ui/game.js
+++ earth_kingdom_v2/ui/game.js
@@ -12,38 +12,10 @@ window.UnitPortraitVisualRemaps = {
         unitType: 'UNIT_PHALANX',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_FOLC_TEUTOBOD': { //your unit type
-        unitType: 'UNIT_LOGIOS_SOCRATES',
+    'UNIT_CABBAGE_MERCHANT': { //your unit type
+        unitType: 'UNIT_MANDARIN',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_FOLC_ARMINIUS': { //your unit type
-        unitType: 'UNIT_LOGIOS_XENOPHON',
-        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
-    },  
-    'UNIT_FOLC_VIBILIUS': { //your unit type
-        unitType: 'UNIT_LOGIOS_PYTHAGORAS',
-        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
-    },
-    'UNIT_FOLC_CIVILIS': { //your unit type
-        unitType: 'UNIT_LOGIOS_PLATO',
-        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
-    },    
-    'UNIT_FOLC_VELEDA': { //your unit type
-        unitType: 'UNIT_TJATY_NEBET',
-        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
-    },      
-    'UNIT_FOLC_PIPARA': { //your unit type
-        unitType: 'UNIT_LOGIOS_HYPATIA',
-        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
-    }, 
-    'UNIT_FOLC_HERMANAFRID': { //your unit type
-        unitType: 'UNIT_TJATY_IMHOTEP',
-        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
-    },   
-    'UNIT_FOLC_BOIORIX': { //your unit type
-        unitType: 'UNIT_TJATY_RAMOSE',
-        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
-    },  
  
 }
 
